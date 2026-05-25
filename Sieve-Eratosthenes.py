16d830af0efabb39e55c935fe90c25d801772ad9
parth-18-bansal <parthbansalcoding@gmail.com>
2026-05-25 16:25:49 +0530

    use the bytearray

--- Sieve-Eratosthenes.py
+++ Sieve-Eratosthenes.py
@@ -2,15 +2,17 @@ import time
 start = time.perf_counter()
 
 n = 600851475143
-prime_arr = [True] * (n+1)
+#n = 16
+#n = 13195
+prime_arr = bytearray([1]) * (n+1)
 
-prime_arr[0] = False
-prime_arr[1] = False
+prime_arr[0] = 0
+prime_arr[1] = 0
 
 for i in range(2,(n+1)):
-    if(prime_arr[i] and (i*i)<=n):
+    if((prime_arr[i] == 1) and (i*i)<=n):
         for j in range(i*i, n+1, i):
-            prime_arr[j] = False
+            prime_arr[j] = 0
 
 end = time.perf_counter()
 print(f"Execution time: {end - start:.6f} seconds")
