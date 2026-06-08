22ceee917e3b3d9e1aaa3f335f85f6bafcd6e0b1
parth-18-bansal <parthbansalcoding@gmail.com>
2026-06-08 16:07:06 +0530

    palindrom largest

--- palindrome_largest.py
+++ palindrome_largest.py
@@ -1,51 +1,47 @@
-a = 10
-b = 10
+import math
+import time
+start = time.perf_counter()
 
-lp = 0
+largest_prime = 0
 
-def palindrome(x):
-    digits = []
+# def palindrome(x):
+#     digits = []
 
-    p = 0
+#     n = x
 
-    n = x
+#     while(n!=0):
+#         i = n // 10
+#         d = n - i*10
+#         digits.append(d)
+#         n = i
 
-    while(n!=0):
-        i = n // 10
-        d = n - i*10
-        digits.append(d)
-        n = i
+#     i = 0
+#     j = len(digits) - 1
 
-    i = 0
-    j = len(digits) - 1
+#     while (i<j):
+#         if(digits[i] == digits[j]):
+#             i = i + 1
+#             j = j - 1
 
-    while (i<j):
-        if(digits[i] == digits[j]):
-            i = i + 1
-            j = j - 1
+#         else:
+#             return False
 
-        else:
-            p = -1
-            break
+#     return True
 
-    if(p == 0):
-        if( x > lp ):
-            lp = x
+def palindrome_str(x):
+    return str(x) == str(x)[::-1]
 
-while (a<100):
-    while (b<100):
+for a in range(100,1000):
+    for b in range(100,1000):
         x = a * b
-        palindrome(x)
-        b = b + 1
+        if (palindrome_str(x)):
+            if (x > largest_prime):
+                largest_prime = x
 
-    a = a + 1
+print(largest_prime)
 
-print(lp)
+end = time.perf_counter()
+print(f"Execution time: {end - start:.6f} seconds")
 
-
-
-
-# for i in digits:
-#     print(i)
 
 
